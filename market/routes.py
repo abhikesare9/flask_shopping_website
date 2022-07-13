f33a2ef00726e31a43b91b6530815ce5470d4ddc
abhikesare9 <abhikesare9@gmail.com>
2022-07-13 19:09:04 +0530

    updated features

--- market/routes.py
+++ market/routes.py
@@ -3,10 +3,11 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user,logout_user
+from flask_login import login_user,logout_user,login_required
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home_page():
     return render_template('home.html')
 
@@ -24,6 +25,8 @@ def register_page():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f'logged in as {user_to_create.username}',category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
