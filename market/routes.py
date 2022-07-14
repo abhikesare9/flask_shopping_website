64ceb5836322e8cc86163315d7a8d3c5027b7c9d
abhikesare9 <abhikesare9@gmail.com>
2022-07-14 21:59:59 +0530

    completed basic code

--- market/routes.py
+++ market/routes.py
@@ -28,6 +28,17 @@ def market_page():
             else:
                 flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}!", category='danger')
 
+        
+        #selling items
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f"Congratulations! You sold {s_item_object.name} back to market!", category='success')
+            else:
+                flash(f"Something went wrong with selling {s_item_object.name}", category='danger')
+
         return redirect(url_for('market_page'))
 
     if request.method == "GET":
@@ -37,6 +48,7 @@ def market_page():
         if items.first() == None:
             flash("You have bought all the items we will know once new items comes up",category='info')
         return render_template('market.html', items=items, purchase_form=purchase_form,owned_items=owned_items,selling_form=selling_form)
+
 
 
 @app.route('/register', methods=['GET', 'POST'])
